eda8d8561999cb717654658894583dc63be9db77
king-ojay <o.attah@alustudent.com>
2025-01-14 10:31:23 +0000

    base_model.py

---
+++ models/base_model.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import uuid 
+from datetime import datetime
+  
+
+class BaseModel:
+    """
+The BaseModel class is the parent class that the other classes inherit from.
+It defines all the common attributes for all the other classes"""
+
+
+def __init__
+
