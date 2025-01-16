0e5b5174204ffbf69b616a0e48211eebceca6661
eddie leftie <59047760+uwambajeddy@users.noreply.github.com>
2025-01-16 09:24:50 +0200

    fix user reload test

--- tests/test_save_reload_user.py
+++ tests/test_save_reload_user.py
@@ -12,8 +12,8 @@ for obj_id in all_objs.keys():
 
 print("-- Create a new User --")
 my_user = User()
-my_user.first_name = "Eddy"
-my_user.last_name = "Airbnb"
+my_user.first_name = "Betty"
+my_user.last_name = "Bar"
 my_user.email = "airbnb@mail.com"
 my_user.password = "root"
 my_user.save()
@@ -21,7 +21,7 @@ print(my_user)
 
 print("-- Create a new User 2 --")
 my_user2 = User()
-my_user2.first_name = "Attah"
+my_user2.first_name = "John"
 my_user2.email = "airbnb2@mail.com"
 my_user2.password = "root"
 my_user2.save()
